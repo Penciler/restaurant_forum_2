e2d87adefdf92889b450c254d1be509758366a91
penciler <eraser41@livemail.tw>
2018-01-06 12:15:20 +0800

    add set_category method and refactor Admin::CategoriesController

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,7 @@
 class Admin::CategoriesController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin
+  before_action :set_category, only: [:update, :destroy]
 
   def index
    @categories = Category.all
@@ -24,7 +25,6 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def update
-    @category = Category.find(params[:id])
     if @category.update(category_params)
       redirect_to admin_categories_path
       flash[:notice] = "category was successfully updated"
@@ -35,7 +35,6 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def destroy
-    @category = Category.find(params[:id])
     @category.destroy
     flash[:alert] = "category was successfully deleted"
     redirect_to admin_categories_path
@@ -48,5 +47,8 @@ class Admin::CategoriesController < ApplicationController
     params.require(:category).permit(:name)
   end
 
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
 end
